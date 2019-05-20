a9090ab5e24ee4e9b393d1843f00304b7ef60063
Yuchen <marc.huai@yahoo.com>
2019-05-20 12:05:35 -0700

    Adds a python script that prints a message greeting capstone project course

---
+++ capstone_code.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# Notebook for the capstone project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as mp
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
